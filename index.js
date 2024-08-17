7e81ec2643d3b729877b1f39621fe6fbc5b57e87
Libin Seban <libinseban97@gmail.com>
2024-08-18 02:22:58 +0530

    new changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cors({
     origin: [
-        "https://frondend-ij4nxzqbx-libin-sebans-projects.vercel.app", "http://localhost:5173/"
+        "https://frondend-gdvt2329p-libin-sebans-projects.vercel.app/", "http://localhost:5173/"
     ],
     credentials: true,
 
