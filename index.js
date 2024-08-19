1be7c1b2b74e7261934cf26c935cbaa1f177f150
Libin Seban <libinseban97@gmail.com>
2024-08-19 11:49:18 +0530

    changed frondend url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // CORS configuration
 app.use(cors({
     origin: [
-        "https://frondend-gdvt2329p-libin-sebans-projects.vercel.app",
+        "https://frondend-e840jg4ou-libin-sebans-projects.vercel.app",
         "http://localhost:5173"
     ],
     credentials: true,
